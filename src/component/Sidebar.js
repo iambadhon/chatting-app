963cf9d43f4f4d5f021ab37908d8329ea3ccdff1
Toufiqul Islam Badhon <toufiqul.dev@gmail.com>
2023-01-18 23:17:55 +0600

    add

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -20,20 +20,20 @@ const Sidebar = ({ active }) => {
     });
   };
   return (
-    <div className="bg-primary w-full lg:px-5 xl:px-11 p-2 lg:py-9 lg:rounded-3xl overflow-y-hidden lg:overflow-x-hidden fixed bottom-0 left-0 z-10 lg:static flex justify-center lg:flex-col gap-x-5 sml:gap-x-16 md:!gap-x-28">
-      <div className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] lg:w-[100px] lg:h-[100px] rounded-full overflow-hidden">
+    <div className="bg-primary w-full px-2 p-2 lg:py-9 lg:rounded-3xl overflow-y-hidden lg:overflow-x-hidden fixed bottom-0 left-0 z-10 lg:static flex justify-center lg:flex-col gap-x-5 sml:gap-x-16 md:!gap-x-28">
+      <div className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] lg:w-[100px] lg:h-[100px] rounded-full overflow-hidden lg:mx-auto">
         <picture>
           <img className="w-full h-full" src={auth.currentUser.photoURL} />
         </picture>
       </div>
-      <h2 className="font-nunito text-xl font-bold text-white text-center mt-2">
+      <h2 className="absolute top-[155px] left-4 xl:left-9 w-[140px] hidden lg:block font-nunito text-xl font-bold text-white text-center">
         {auth.currentUser.displayName}
       </h2>
       <div className="flex flex-row lg:flex-col items-center gap-y-20 gap-x-5 sml:gap-x-6 md:!gap-x-11 lg:mt-24">
         <div
           className={`${
             active == "home" &&
-            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-10 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
+            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-9 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
           }`}
         >
           <Link to="/">
@@ -47,7 +47,7 @@ const Sidebar = ({ active }) => {
         <div
           className={`${
             active == "message" &&
-            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-10 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
+            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-9 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
           }`}
         >
           <Link to="/message">
@@ -61,7 +61,7 @@ const Sidebar = ({ active }) => {
         <div
           className={`${
             active == "notification" &&
-            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-10 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
+            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-9 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
           }`}
         >
           <Link to="/notification">
@@ -75,7 +75,7 @@ const Sidebar = ({ active }) => {
         <div
           className={`${
             active == "setting" &&
-            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-10 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
+            "z-10 px-1 py-3 lg:py-4 xl:py-5 lg:px-10 relative after:absolute lg:after:top-0 after:left-0 lg:after:left-3 xl:after:left-0 after:bottom-0 after:content-[''] after:bg-white after:w-full after:h-[120%] lg:after:w-[125%] lg:after:h-full after:z-[-1] after:rounded-b-xl lg:after:rounded-none lg:after:rounded-l-3xl before:absolute before:-top-2.5 md:before:-top-3.5 lg:before:top-0 before:right-0 lg:before:-right-4 xl:before:-right-9 before:content-[''] before:bg-primary before:w-full before:h-[15%] lg:before:w-[15%] lg:before:h-full before:rounded-b-xl lg:before:rounded-none lg:before:rounded-l-xl"
           }`}
         >
           <Link to="/setting">
